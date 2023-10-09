9de2aefe803e683df1f5d2c7c18e4d8dc43ada07
0102johnny <rt2824.recruit.1022@gmail.com>
2023-10-09 21:39:13 +0900

    [add] 画面中央のボタンをタップするとログイン&サインアップ画面に遷移する処理を実装

--- PracticeFirebaseUI/ViewController.swift
+++ PracticeFirebaseUI/ViewController.swift
@@ -6,13 +6,38 @@
 //
 
 import UIKit
+import FirebaseOAuthUI
+import FirebaseGoogleAuthUI
 
 
-class ViewController: UIViewController {
+
+final class ViewController: UIViewController, FUIAuthDelegate {
+
+    @IBAction func didTapAuthVCButton(_ sender: Any) {
+        showAuthVC()
+    }
+    private var authUI = FUIAuth.defaultAuthUI()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpFirebaseUI()
     }
 
+    private func setUpFirebaseUI() {
+        guard let authUI = authUI else { fatalError("AuthUI Error") }
+        authUI.delegate = self
+        authUI.providers = [
+            FUIGoogleAuth(authUI: authUI),
+            FUIOAuth.twitterAuthProvider(),
+            FUIOAuth.appleAuthProvider()
+        ]
+    }
+
+    private func showAuthVC() {
+        guard let authUI = authUI else { fatalError("AuthUI Error") }
+        let authVC = authUI.authViewController()
+        present(authVC, animated: true)
+    }
 
 }
